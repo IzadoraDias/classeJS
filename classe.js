368da0326b7089f9d2f868c635a87cb7a5610335
IzadoraDias <izzadoradias@gmail.com>
2018-11-06 13:25:17 -0200

    atualização exemplo classe Ponto

--- classe.js
+++ classe.js
@@ -74,8 +74,21 @@ class Ponto {
   mover_verticalmente(altura) {
     this.y = (this.y + altura)
     return this.y
-
   }
+  mover(distanciaX, distanciaY) {
+    this.x += distanciaX
+    this.y += distanciaY
+    return [this.x, this.y]
+  }
+
+  distancia_entre_dois_pontos(outro_ponto) {
+    const x1 = this.x
+    const y1 = this.y
+    const x2 = outro.ponto.x
+    const y2 = outro_ponto.y
+    return Math.sqrt( (Math.abs( x1 - x2 )) ** 2 + ( Math.abs( y1 - y2 )) ** 2)
+  }
+
 }
 
 class Rect {
